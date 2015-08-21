71ddc9619651a1e8ff6fcff402bfc8005e35362f
liyunlong1123 <liyunlong3189@163.com>
2015-08-21 21:51:36 +0800

    init project

---
+++ QQBadgeView/QQBadgeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQBadgeView
+//
+//  Created by 李云龙 on 15/8/21.
+//  Copyright (c) 2015年 hihilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QQBadgeView/QQBadgeView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QQBadgeView
+//
+//  Created by 李云龙 on 15/8/21.
+//  Copyright (c) 2015年 hihilong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
